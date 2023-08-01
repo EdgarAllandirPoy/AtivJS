92f3e9f78ff7b15568c1dbd6e3fd023c4383f09e
EdgarAllandirPoy <edgar.F.A2007@gmail.com>
2023-08-01 16:39:49 -0300

    a

---
+++ Legionari III.js
@@ -0,0 +1,9 @@
+
+
+
+function Area() {
+    let x = Number(prompt("Informe o valor X"));
+    let y = Number(prompt("Informe o valor Y"));
+    let resultado = x * y;
+    alert("O resultado da área é " + resultado + "m²");
+}
